ff9ce389dd38fe2f0def513412e70ff9b9a8c323
Charles Jackson <cjac803@UoA.auckland.ac.nz>
2019-07-20 13:02:34 +1200

    Alexa helper now keeps a history of changes to state, useful for debugging.

--- alexa_helper.py
+++ alexa_helper.py
@@ -7,19 +7,41 @@ from ask_sdk_model.ui import SimpleCard
 from ask_sdk_model import Response
 
 class StateVariables:
+    STATE_HISTORY_ENABLED = True # useful for debugging
+    STATE_HISTORY = "STATE_HISTORY"
+
     @staticmethod
     def set_state(handler_input, key, value):
         # retireve Alexa state variables
         state_variables = handler_input.attributes_manager.persistent_attributes
 
-        # store the value in the state variables and save to Alexa session
-        state_variables[str(key)] = value
+        # make sure that the key is a string
+        key = str(key)
+
+        # store the value in the state variables
+        state_variables[key] = value # note this overrides any previous value if it existed
+        
+        if (StateVariables.STATE_HISTORY_ENABLED):
+            # get the state history
+            state_histroy = state_variables[StateVariables.STATE_HISTORY]
+
+            # create the history if it doesn't exist yet
+            if (state_histroy is None):
+                state_histroy = list()
+
+            # add the state we just set
+            state_histroy.append({ key, value, })
+
+            # write history back to state variables list
+            state_variables[StateVariables.STATE_HISTORY] = state_histroy
+
+        # save the state variables
         handler_input.attributes_manager.session_attributes = state_variables
 
     @staticmethod
     def retrieve_non_null_state(handler_input, key):
         #read out our key, if it's None raise an error.
-        value = StateVariables.retrieve_key(handler_input, key)
+        value = StateVariables.retrieve_state(handler_input, key)
         if (value is None):
             raise Exception("reading out variable '" + str(key) + "' returned None.") #TODO proper exception type
         else:
