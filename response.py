07cf09d2eb555803f281607b5b550e1a2c1ca175
Charles Jackson <cjac803@UoA.auckland.ac.nz>
2019-07-21 19:27:19 +1200

    resopsne

--- response.py
+++ response.py
@@ -1,9 +1,10 @@
+from audio import Audio
 
 class Response() :
     def __init__ (self, speech_text, state_variables=None, reprompt=None, should_save_speech_text=None):
         # state_variables is fine to be None
         if (reprompt is None):
-            reprompt = "Repeat yourself"
+            reprompt = Audio.FORMAT_STRING.format("19_Error.mp3")
         if (should_save_speech_text is None):
             should_save_speech_text = True
         
